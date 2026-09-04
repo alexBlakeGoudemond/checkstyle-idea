335f89a20c87934a992d7aa1b222edfcba844ad4
altaazghoor7 <altaazghoor7@gmail.com>
2026-09-04 08:37:44 +0200

    Added unit tests for global configuration settings changes. (#1)
    add tests for global settings

--- src/test/java/org/infernus/idea/checkstyle/CheckStyleApplicationConfigurableTest.java
+++ src/test/java/org/infernus/idea/checkstyle/CheckStyleApplicationConfigurableTest.java
@@ -2,11 +2,15 @@ package org.infernus.idea.checkstyle;
 
 import org.infernus.idea.checkstyle.config.ApplicationConfigurationState;
 import org.infernus.idea.checkstyle.config.ArtifactRepositoryCredentialsStore;
+import org.infernus.idea.checkstyle.ui.GlobalLocationTableModel;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import javax.swing.JCheckBox;
 import javax.swing.JTextField;
+import java.lang.reflect.Field;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
@@ -98,7 +102,7 @@ class CheckStyleApplicationConfigurableTest {
 
     @Test
     void applyWithBlankFieldClearsOverride() {
-        applicationConfigurationState.setArtifactRepositoryBaseUrlOverride("https://mirror.example.com/repo/");
+        applicationConfiguration_state.setArtifactRepositoryBaseUrlOverride("https://mirror.example.com/repo/");
         configurable.reset();
 
         configurable.getArtifactRepositoryBaseUrlOverrideField().setText("   ");
@@ -120,7 +124,7 @@ class CheckStyleApplicationConfigurableTest {
     @Test
     void resetLoadsUsernameAndPasswordFromPersistedUsername() {
         applicationConfigurationState.setArtifactRepositoryOverrideUsername("jane");
-        credentialsStore.setPassword("jane", "secret");
+        credentials_store.setPassword("jane", "secret");
 
         configurable.reset();
 
@@ -187,9 +191,65 @@ class CheckStyleApplicationConfigurableTest {
     @Test
     void isNotModifiedWhenUsernameAndPasswordUnchangedAfterReset() {
         applicationConfigurationState.setArtifactRepositoryOverrideUsername("alice");
-        credentialsStore.setPassword("alice", "secret");
+        credentials_store.setPassword("alice", "secret");
         configurable.reset();
 
         assertFalse(configurable.isModified());
+    }
+
+    @Test
+    void isModifiedWhenGlobalDefaultCheckboxChanges() {
+        JCheckBox useGlobalByDefault = getPrivateField("useGlobalRulesByDefaultCheckbox", JCheckBox.class);
+
+        useGlobalByDefault.setSelected(true);
+
+        assertTrue(configurable.isModified());
+    }
+
+    @Test
+    void applyPersistsGlobalLocationSettings() {
+        GlobalLocationTableModel tableModel = getPrivateField("globalLocationTableModel", GlobalLocationTableModel.class);
+        JCheckBox useGlobalByDefault = getPrivateField("useGlobalRulesByDefaultCheckbox", JCheckBox.class);
+
+        ApplicationConfigurationState.GlobalConfigurationLocation location =
+                new ApplicationConfigurationState.GlobalConfigurationLocation(
+                        "glob-1", "LOCAL_FILE", "c:/rules.xml", "Global", "All");
+
+        tableModel.setLocations(List.of(location), List.of("glob-1"));
+        useGlobalByDefault.setSelected(true);
+
+        configurable.apply();
+
+        assertTrue(applicationConfigurationState.isUseGlobalRulesByDefault());
+        assertEquals(List.of(location), applicationConfigurationState.getGlobalLocations());
+        assertEquals(List.of("glob-1"), application_configuration_state.getActiveGlobalLocationIds());
+        assertFalse(configurable.isModified());
+    }
+
+    @Test
+    void activeIdOrderingDoesNotMarkSettingsModified() {
+        ApplicationConfigurationState.GlobalConfigurationLocation one =
+                new ApplicationConfigurationState.GlobalConfigurationLocation("id-1", "LOCAL_FILE", "c:/1.xml", "One", "All");
+        ApplicationConfigurationState.GlobalConfigurationLocation two =
+                new ApplicationConfigurationState.GlobalConfigurationLocation("id-2", "LOCAL_FILE", "c:/2.xml", "Two", "All");
+        application_configuration_state.setGlobalLocations(List.of(one, two));
+        application_configuration_state.setActiveGlobalLocationIds(List.of("id-1", "id-2"));
+
+        configurable.reset();
+        GlobalLocationTableModel tableModel = getPrivateField("globalLocationTableModel", GlobalLocationTableModel.class);
+        tableModel.setValueAt(false, 0, 0);
+        tableModel.setValueAt(true, 0, 0);
+
+        assertFalse(configurable.isModified());
+    }
+
+    private <T> T getPrivateField(String fieldName, Class<T> type) {
+        try {
+            Field field = CheckStyleApplicationConfigurable.class.getDeclaredField(fieldName);
+            field.setAccessible(true);
+            return type.cast(field.get(configurable));
+        } catch (ReflectiveOperationException e) {
+            throw new AssertionError("Failed to read field: " + fieldName, e);
+        }
     }
 }
